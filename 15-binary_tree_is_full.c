fe54d37534e35c87b2e0088e63cbcf2d67368e1d
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-14 16:48:17 +0300

    edit code to includ return null when flull

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,25 +1,25 @@
 #include "binary_trees.h"
-#include <stdbool.h>
 
 /**
 * binary_tree_is_full - Checks if a binary tree is full.
 * A full binary tree is a tree where every node has either 0 or 2 children.
-*
 * @tree: Pointer to the root node of the tree to check.
 *
-* Return: 1 if the tree is full, 0 otherwise.
+* Return: 1 if the tree is full, 0 if not or if tree is NULL.
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
+		return (0);
 
+	/* A leaf node is full */
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
+	/* If both left and right children are present */
 	if (tree->left != NULL && tree->right != NULL)
-		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
+	/* If one child is missing */
 	return (0);
 }
